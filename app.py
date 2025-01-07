e02fdef44052083798fa80294f6b8bbf6eb3cdf5
Jumana pa <jumana.pa@imaginist.ai>
2025-01-07 09:30:10 +0530

    Authentication using JWT

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask,jsonify,request
-
+from flask_jwt_extended import JWTManager,create_access_token,jwt_required,get_jwt_identity
 app=Flask(__name__)
+app.config['JWT_SECRET_KEY'] = 'my-strong-secret-key'
+jwt = JWTManager(app)
+
 
 users=[
     {"id": 1, "name": "Jumi","email": "jumi@example.com"},
@@ -14,11 +17,23 @@ products=[
     {"id" : 2, "name": "shirt", "category": "cloth","price": 200},
 ]
 
+@app.route('/login',methods=['POST'])
+def login():
+    username=request.json.get('username')
+    password=request.json.get('password')
+    if username == "admin" and password == "admin":
+        token=create_access_token(identity=username)
+    
+        return jsonify({"access_token": token})
+    return {"error":"Invalid credentials"},401
+
+
 @app.route('/users',methods=['GET'])
 def get_user():
     return jsonify(users)
 
 @app.route('/users',methods=['POST'])
+@jwt_required()
 def add_user():
     data=request.json
     if "id" not in data or "name" not in data or "email" not in data:
@@ -30,7 +45,8 @@ def add_user():
     users.append(data)
     return jsonify(data), 201
 
-@app.route('/user/<int:user_id>',methods=['PUT'])
+@app.route('/user/<int:user_id>',methods=['PUT'],endpoint="update_user")
+@jwt_required()
 def update_user(user_id):
     for user in users:
         if user['id'] == user_id:
@@ -38,7 +54,8 @@ def update_user(user_id):
             return jsonify(user)
     return "Not Found Error" , 404
 
-@app.route('/user/<int:user_id>',methods=['DELETE'])
+@app.route('/user/<int:user_id>',methods=['DELETE'],endpoint="delete_user")
+@jwt_required()
 def delete_user(user_id):
     global users
     users=[user for user in users if user['id'] != user_id]
@@ -47,7 +64,7 @@ def delete_user(user_id):
 @app.route('/user/<int:user_id>',methods=['GET'])
 def get_user_by_id(user_id):
     for user in users:
-        if user[id] == user_id:
+        if user['id'] == user_id:
             return jsonify(user)
     return "Not Found User", 404
 
@@ -56,18 +73,20 @@ def get_user_by_id(user_id):
 def get_product():
     return jsonify(products)
 
-@app.route('/products',methods=['POST'])
+@app.route('/products',methods=['POST'],endpoint="add_product")
+@jwt_required()
 def add_product():
     data=request.json
     if 'id' not in data or 'name' not in data or 'price' not in data:
-        return {"error":"Id ,Name ,Category and Price are required fields"},400
+        return {"error":"Id ,Name  and Price are required fields"},400
     product_exist=[product for product in products if product['id'] == data['id']]
     if product_exist:
         return {"error":"Id already exist"},400
     products.append(data)
     return jsonify(data), 201
 
-@app.route('/product/<int:product_id>',methods=['PUT'])
+@app.route('/product/<int:product_id>',methods=['PUT'],endpoint="update_product_by_price")
+@jwt_required()
 def update_product_price(product_id):
     for product in products:
         if product['id'] == product_id:
@@ -75,13 +94,14 @@ def update_product_price(product_id):
             return jsonify(product)
     return "Not Found Error" , 404
 
-@app.route('/product/<int:product_id>',methods=['DELETE'])
+@app.route('/product/<int:product_id>',methods=['DELETE'],endpoint="delete_product")
+@jwt_required()
 def delete_product(product_id):
     global products
     products=[product for product in products if product['id'] != product_id]
     return f"Product deleted Succesfully"
 
-@app.route('/product/<int:product_id>',methods=['GET'])
+@app.route('/product/<int:product_id>',methods=['GET'],endpoint="get_product_by_id")
 def get_product_by_id(product_id):
     for product in products:
         if product['id'] == product_id:
